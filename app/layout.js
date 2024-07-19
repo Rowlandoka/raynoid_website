24a7e288c026b3d4d11f3e422c9e2f1606150268
Rowland Oka <rowlandoka@gmail.com>
2024-07-19 15:36:30 +0100

    complete the home page and navigagtion menu

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,32 @@
-import Logo from './components/Logo';
-import Navigation from './components/Navigation';
+import '@/app/_styles/globals.css';
+import Header from '@/starter/components/Header';
 
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+});
 export const metadata = {
-	title: 'The Wild Oasis',
+	title: {
+		template: '%s / The Wild Oasis',
+		default: 'Welcome / The Wild Oasis',
+	},
+	description:
+		'Luxurious cabin hotel, located in the heart of the Itailan Dolomites, surrounded by beautiful mountains and dark forests.',
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body>
-				<header>
-					<Logo />
-					<Navigation />
-				</header>
+			<body
+				className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+			>
+				<Header />
 
-				<main>{children}</main>
-				<footer>
-					<p>&copy; 2024 The Wild Oasis. All rights reserved.</p>
-				</footer>
+				<div className='flex-1 px-8 py-12'>
+					<main className='max-w-7xl mx-auto'>{children}</main>
+				</div>
 			</body>
 		</html>
 	);
