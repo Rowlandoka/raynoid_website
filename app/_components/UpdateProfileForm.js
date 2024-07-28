aa465ee5ab4956e3491f0dd5c9f3ccb671d342e3
Rowland Oka <rowlandoka@gmail.com>
2024-07-28 13:04:33 +0100

    complete guest profile update

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,17 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { updateGuest } from '../_lib/actions';
 
-export default function UpdateProfileForm({ children }) {
+export default function UpdateProfileForm({ guest, children }) {
 	const [count, setCount] = useState();
 
-	// CHANGE
-	const countryFlag = 'pt.jpg';
-	const nationality = 'Nigeria';
+	const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
 	return (
-		<form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
+		<form
+			action={updateGuest}
+			className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'
+		>
 			<div className='space-y-2'>
 				<label>Full name</label>
 				<input
+					defaultValue={fullName}
+					name='fullName'
 					disabled
 					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
 				/>
@@ -20,7 +27,9 @@ export default function UpdateProfileForm({ children }) {
 			<div className='space-y-2'>
 				<label>Email address</label>
 				<input
+					defaultValue={email}
 					disabled
+					name='email'
 					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
 				/>
 			</div>
@@ -40,16 +49,27 @@ export default function UpdateProfileForm({ children }) {
 			<div className='space-y-2'>
 				<label htmlFor='nationalID'>National ID number</label>
 				<input
+					defaultValue={nationalID}
 					name='nationalID'
 					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
 				/>
 			</div>
 
 			<div className='flex justify-end items-center gap-6'>
-				<button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-					Update profile
-				</button>
+				<Button />
 			</div>
 		</form>
+	);
+}
+
+function Button() {
+	const { pending } = useFormStatus();
+	return (
+		<button
+			className='bg-accent-500 rounded-md px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+			disabled={pending}
+		>
+			{pending ? 'Updating...' : 'Update profile'}
+		</button>
 	);
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,35 @@
 'use server';
 
-import { signIn } from './auth';
+import { revalidatePath } from 'next/cache';
+import { auth, signIn, signOut } from './auth';
+import { supabase } from './supabase';
+
+export async function updateGuest(formData) {
+	const session = await auth();
+	if (!session) throw new Error('You must be signed in to update your profile');
+
+	const nationalID = formData.get('nationalID');
+	const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+	if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+		throw new Error('Please provide a valid national ID number');
+
+	const updateData = { nationality, nationalID, countryFlag };
+
+	const { data, error } = await supabase
+		.from('guests')
+		.update(updateData)
+		.eq('id', session.user.guestId);
+
+	if (error) throw new Error('Guest could not be updated');
+
+	revalidatePath('/account/profile');
+}
 
 export async function signInAction() {
 	await signIn('google', { redirectTo: '/account' });
+}
+
+export async function signOutAction() {
+	await signOut({ redirectTo: '/' });
 }
